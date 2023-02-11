5bcb6a5293c89ca99db415605f0b3c14b25950e2
RomahaVO <romahabodnaruk@icloud.com>
2023-02-11 23:01:23 +0200

    fin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,99 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import searchImages from './js/fetchImages';
+import searchImg from './js/fetchImages';
+import infiniteScroll from 'infinite-scroll';
+
+const form = document.getElementById('search-form');
+const galleryImages = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+form.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', loadMoreBtnClick);
 
 
+function onSubmit (e) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    searchImg.galleryEl.query = form.elements.searchQuery.value.trim();
+
+    loadMoreBtn.classList.remove('hidden');
+
+    if(searchImg.galleryEl.query === ''){
+        galleryImages.innerHTML = "";
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        loadMoreBtn.classList.add('hidden');
+        return;
+    };
+
+    searchImg.fetchImages(searchImg.galleryEl.query).then(items=>{
+        if (items.length === 0){
+        imagesList.innerHTML = "";
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        loadMoreBtn.classList.add('hidden');
+        return;
+        }
+        console.log(items);
+    });
+
+    cleanerMarkup(galleryImages);
+    searchImg.galleryEl.imgPage = 1;
+    loadMoreBtnClick();
+    
+};
+
+function createImageGallery(hits) {
+    const markup = hits.map(hit => `<div class="photo-card">
+    <a href="${hit.largeImageURL}"><img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" width="400px" height="244px" style="object-fit:cover;"/></a>
+    <div class="info">
+    <p class="info-item">
+        <b>Likes <br>${hit.likes}</b>
+    </p>
+    <p class="info-item">
+        <b>Views <br>${hit.views}</b>
+    </p>
+    <p class="info-item">
+        <b>Comments <br>${hit.comments}</b>
+    </p>
+    <p class="info-item">
+        <b>Downloads <br>${hit.downloads}</b>
+    </p>
+    </div>
+</div>`).join('');
+
+galleryImages.insertAdjacentHTML("beforeend", markup);
+};
+
+
+function loadMoreBtnClick(){
+    loadMoreBtn.disabled=true;
+    loadMoreBtn.textContent='Loading...';
+    searchImg.fetchImages(searchImg.galleryEl.query).then(items=>{
+        searchImg.galleryEl.imgPage += 1;
+        console.log(items);
+        createImageGallery(items);
+        gallery.refresh();
+        loadMoreBtn.disabled=false;
+        loadMoreBtn.textContent='Load more';
+    }).catch(error=>{
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        loadMoreBtn.classList.add('hidden');
+    })
+};
+
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+});
+
+function cleanerMarkup(element) {
+    return  element.innerHTML = '';
+};
+
+// let infScroll = new InfiniteScroll( galleryImages, {
+//     path: '.pagination__next',
+//     append: '.post',
+//     history: false,
+// });

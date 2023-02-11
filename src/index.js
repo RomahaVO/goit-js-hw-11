7bede956bfc58f6dc7c0e3f6ae878ec74eeee61e
RomahaVO <romahabodnaruk@icloud.com>
2023-02-11 18:04:53 +0200

    import

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import searchImages from './js/fetchImages';
+
+
---
+++ src/js/fetchImages.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const galleryEl = {
+    imgPage: 1,
+    query: '',
+};
+
+const ENDPOINT = 'https://pixabay.com/api/';
+const API_KEY = '33561476-20a78efec667bb0e7bc12460b';
+
+async function fetchImages(){
+    const response = await axios.get(`${ENDPOINT}?key=${API_KEY}&q=${galleryEl.query}&image_type=photo$orientation=horizontal&safesearch=true&per_page=40&page=${galleryEl.imgPage}`);
+    return response.data.hits;
+};
+
+export default { galleryItems, fetchImages };
